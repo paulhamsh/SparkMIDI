8438b0550078ba36a8a3c36024f3af5243afda68
paulhamsh <p.w.hamshere@gmail.com>
2022-01-16 17:27:27 +0000

    Update MIDI.h

--- SparkMIDI8/MIDI.h
+++ SparkMIDI8/MIDI.h
@@ -1,19 +1,23 @@
 #ifndef MIDI_h
 #define MIDI_h
 
+#ifdef USB_HOST_ON
 #include <usbh_midi.h>
 #include <SPI.h>
-
-HardwareSerial *ser;
-HardwareSerial *ser2;
 
 USB Usb;
 USBH_MIDI Midi(&Usb);
 bool usb_connected;
-
 uint16_t rcvd;
 uint8_t chan;
 uint8_t midi_buf[50*4];
+#endif
+
+#ifdef USB_TRINKET
+HardwareSerial *ser2;
+#endif
+
+HardwareSerial *ser;
 
 int ble_app_status;
 int ble_app_cmd_count;
